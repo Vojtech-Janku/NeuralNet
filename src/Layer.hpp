50e27e39b4185e7d3e076b12f4bc98bbb14cf70e
Vojtech Janku <vojtech.janku@oracle.com>
2025-11-25 16:40:19 +0100

    moved the activation functions map to Layer.hpp

--- src/Layer.hpp
+++ src/Layer.hpp
@@ -1,3 +1,4 @@
+#include <map>
 #include <vector>
 
 using namespace std;
@@ -44,3 +45,23 @@ string get_str( Activation a ) {
     default:                        return "Unknown";
     }
 }
+
+// activation functions and their derivatives
+typedef float (*act_fun)(float);
+float step( float x ) { return ( x < 0 ) ? 0 : 1; }
+float relu( float x ) { return ( x < 0 ) ? 0 : x; }
+float leaky_relu( float x ) { return ( x < 0 ) ? x/16 : x; }
+float sigmoid( float x ) { return 1 / ( 1 + exp(-x) ); }  //{ return x / (1 + abs(x)); } // "fast" sigmoid
+float tanh_fun( float x ) { return std::tanh(x); }
+float step_diff( float ) { return 0; }
+float relu_diff( float x ) { return ( x <= 0 ) ? 0 : 1; }
+float leaky_relu_diff( float x ) { return ( x < 0 ) ? 1/16 : 1; }
+float sigmoid_diff( float x ) { return sigmoid(x) * (1 - sigmoid(x) ); }
+float tanh_diff( float x ) { return 1 - std::pow(std::tanh(x), 2); }
+map< Activation, pair<act_fun, act_fun> > activ_functions = {
+    { Activation::STEP,         make_pair(step, step_diff) },
+    { Activation::RELU,         make_pair(relu, relu_diff) },
+    { Activation::LEAKY_RELU,   make_pair(leaky_relu, leaky_relu_diff) },
+    { Activation::SIGMOID,      make_pair(sigmoid, sigmoid_diff) },
+    { Activation::TANH,         make_pair(tanh_fun, tanh_diff) }
+};
