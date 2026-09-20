d220fa2f740d7aa0acee5c9f31c5d68f1b6fc30d
Vojtech Janku <vojta.janku7@seznam.cz>
2026-09-20 19:58:07 +0200

    Created a tensor class

---
+++ src/Tensor.hpp
@@ -0,0 +1,34 @@
+#include <cstddef>
+#include <vector>
+using namespace std;
+
+struct Tensor 
+{
+    vector<float> data;
+    vector<size_t> shape;
+
+    Tensor( vector<size_t> shape ) : shape(shape) {
+        size_t total = 1;
+        for ( auto d : shape ) total *= d;
+        data = vector<float>(total);
+    }
+
+    float &operator[](size_t idx) {return data[idx];}
+    float &at(size_t i, size_t j) {return data[i*shape[1]+j];}
+
+    void operator*(float n) {
+        for (auto &e : data) e *= n;
+    }
+
+    Tensor operator*(Tensor &other) {
+        return Tensor(vector<size_t>(0));   // TODO: fix this
+    }
+
+    int getDimension() {
+        return shape.size();
+    }
+
+    vector<size_t> getShape() {
+        return shape;
+    }
+};
